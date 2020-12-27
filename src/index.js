f34bbd726382e90473e90bda927416a975c18e4b
Mateusz <mateusz.pytel@grupakmk.pl>
2020-12-27 22:37:23 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,57 @@
+/**
+ * This plugin allows to fetch individual customer price from Magento
+ * @param config
+ * @param db
+ * @param router
+ * @param cache
+ * @param apiStatus
+ * @param apiError
+ * @param getRestApiClient
+ * @returns {{router: *, route: string, pluginName: string, domainName: string}}
+ */
 module.exports = ({ config, db, router, cache, apiStatus, apiError, getRestApiClient }) => {
-    console.warn('----> Plugin initialized');
-    
+    const createMage2RestClient = () => {
+        const client = getRestApiClient();
+        client.addMethods('customerPrice', (restClient) => {
+            const module = {};
+            module.get = (customerId, token) => {
+                return restClient.get(`/kmk-customerprice/customerprice/search??searchCriteria%5BfilterGroups%5D%5B0%5D%5Bfilters%5D%5B0%5D%5Bfield%5D=customer_id&searchCriteria%5BfilterGroups%5D%5B0%5D%5Bfilters%5D%5B0%5D%5Bvalue%5D=${encodeURI(customerId)}`)
+            };
+
+            return module;
+        });
+
+        return client;
+    };
+
+    router.get('/:customerId', async (req, res) => {
+        try {
+            const {customerId} = req.params;
+            const {token, storeCode} = req.query;
+            if (!customerId) { throw new Error(`Customer id is required`); }
+
+            const client = createMage2RestClient();
+            cache.get(req, [`customer-price-${customerId}`], client.customerPrice.get, customerId, token)
+                .then(async response => {
+                    try {
+                        const {items} = response;
+                        apiStatus(res, items, 200);
+                    } catch (e) {
+                        apiStatus(res, response, 200);
+                    }
+                })
+                .catch(err => {
+                    apiError(res, err.message || err || `Assortments not found`);
+                })
+        } catch (err) {
+            apiError(res, { code: 401, errorMessage: err.message || err || 'Customer price error' });
+        }
+    });
+
     return {
-        domainName: '{{domainName}}',
-        pluginName: '{{pluginName}}',
-        route: '{{restRoute}}',
+        domainName: '@grupakmk',
+        pluginName: 'customer-price',
+        route: 'customer-price',
         router
     };
 };
